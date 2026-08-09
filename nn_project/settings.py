d1df860ad9e0262a6304c80a902f277695047d12
Arseniy <arseniyqwe777@gmail.com>
2026-08-09 16:26:31 +0300

    Fix: обновлен settings.py для подключения к БД Timeweb

--- nn_project/settings.py
+++ nn_project/settings.py
@@ -64,7 +64,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'default_db',
         'USER': 'gen_user',
-        'PASSWORD': '1ar7s2en70SENYA@#',
+        'PASSWORD': 'a17r2sne07ESYN@A#',
         'HOST': '104.171.139.74',
         'PORT': '5432',
     }
